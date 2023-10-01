3aaf30d08b01fe82b0d1299fac572ddd26c53ea7
zol23-g <zelalemwubet93@gmail.com>
2023-10-01 13:27:23 -0700

    first-commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -225,8 +225,8 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
